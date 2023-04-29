0339b852844d1c45e2308d69b835aec14bae2fbd
ClasherKasten <cfb1470@gmail.com>
2023-04-30 00:15:29 +0200

    finally able to remove snap?

--- modules/desktop/manifests/purged.pp
+++ modules/desktop/manifests/purged.pp
@@ -1,10 +1,13 @@
 class desktop::purged {
   exec { 'remove firefox snap'
-    command => 'sudo snap remove firefox',
-  } ->
+    command => 'sudo systemctl disable var-snap-firefox-common-host\\x2dhunspell.mount && ',
+  }
   $purged = [
     'command-not-found', 'gitk', 'libreoffice-common',
     'printer-driver-foo2zjs', 'snapd',
   ]
-  package {$purged: ensure => 'purged'}
+  package { $purged:
+    ensure   => 'purged',
+    required => Exec['remove firefox snap']
+  }
 }
