ae9a4f590deb458c7e6b1f60b99687406a129eef
ClasherKasten <cfb1470@gmail.com>
2024-05-06 17:11:34 +0200

    port 'upstream' changes

--- modules/packages/manifests/utilities.pp
+++ modules/packages/manifests/utilities.pp
@@ -1,7 +1,7 @@
 class packages::utilities {
   $tools = [
-    'curl', 'dos2unix', 'ffmpeg', 'graphviz', 'inotify-tools', 'jq', 'mlocate',
-    'neofetch', 'rlwrap', 'sqlite3', 'tmux', 'tree',
+    'curl', 'dos2unix', 'ffmpeg', 'graphviz', 'inotify-tools', 'jq',
+    'neofetch', 'ncdu', 'plocate', 'rlwrap', 'sqlite3', 'tmux', 'tree',
     'xclip', 'fd-find', 'fzf', 'cifs-utils'
   ]
   package { $tools: ensure => 'latest' }
